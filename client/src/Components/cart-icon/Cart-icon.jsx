c978fc16665195502182e3be5bfbf11121c88374
Dedalino <kelsonpapelo18@gmail.com>
2022-09-02 21:11:48 +0100

    add count to cart icon

--- client/src/Components/cart-icon/Cart-icon.jsx
+++ client/src/Components/cart-icon/Cart-icon.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './cart-icon.style.css'
 import {ReactComponent as ShoppingIcon} from '../../assets/cart.svg'
+import { useSelector } from 'react-redux'
 
 const CartIcon = () => {
+  const cart = useSelector(state => state.cart)
+  const {cartItems} = cart
+
+  const itemCount = cartItems.reduce((acc, cartItem) => {
+    return acc + cartItem.quantity
+  },0)
+  
   return (
     <div className='cart-icon'>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
   )
 }
